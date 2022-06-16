2d5820368418e2b4210eb86599f5ff0f34a4bdd3
Kabani4 <User@WIN-LIBE7B67LA6>
2022-06-16 22:39:33 +0300

    сделал цвет бирюзовым добавил psd перса

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,7 @@ SDL_Window* window{ nullptr };
 SDL_Surface* screenSurface{ nullptr };
 SDL_Surface* picture{ nullptr };
 
-int SDL_main()
+int SDL_main(int argc, char* argv[])
 {
 	
 
@@ -16,11 +16,11 @@ int SDL_main()
 	{
 		window = SDL_CreateWindow("Text above page", SDL_WINDOWPOS_CENTERED_MASK, SDL_WINDOWPOS_CENTERED_MASK, 640, 480, SDL_WINDOW_OPENGL);
 		screenSurface = SDL_GetWindowSurface(window);
-		SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+		SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0x0, 0xFF, 0xFF));
 		SDL_UpdateWindowSurface(window);
 		SDL_Delay(3000);
 		
-	}//леха гей
+	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
